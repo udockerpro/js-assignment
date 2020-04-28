f236050c21c59ff32f3d74b7fc9a1c894f92ae9f
technicalmrpro <technicalmrpro@gmail.com>
2020-04-28 19:01:34 +0600

    added task4.html and task4.js and change page names task 1-4 .html

---
+++ Assignment3_cnc024937/task4.js
@@ -0,0 +1,12 @@
+
+
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+document.write("<h1> Cities list: </h1> <br>");
+document.write(cities);
+
+var selectedCities = cities.slice(0, 3);
+
+document.write("<br> <br> <h1> Selected cities list:</h1> <br>");
+document.write(selectedCities);
