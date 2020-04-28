88de06f3031d1aa2f43ea1586d20c6684e3dedd6
technicalmrpro <technicalmrpro@gmail.com>
2020-04-28 19:17:15 +0600

    commit again task6

--- Assignment3_cnc024937/task6.js
+++ Assignment3_cnc024937/task6.js
@@ -1,7 +1,11 @@
 
+
+
+
 var aCities = ["Karachi", "Lahore", "Islamabad", "Faisalabad"];
 var o = ["th", "st", "nd", "rd"];
 
-for (i = 0; i < aCities.length -1 ; i++) {
+for (i = 0; i < aCities.length - 1; i++) {
+
     document.write((i + 1) + (o[i + 1]) + " choice is " + aCities[i] + "<br>");
 }
