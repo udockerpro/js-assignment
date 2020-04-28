468e512117e8cbb3d4451c771be6939cc3094a73
technicalmrpro <technicalmrpro@gmail.com>
2020-04-28 16:44:36 +0600

    task1 and task2 added and completed

---
+++ Assignment3_cnc024937/task1.js
@@ -0,0 +1,17 @@
+
+var education = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+
+document.write("<h3>Educations:</h3> <br>")
+document.write("1) " + education[0] + "<br>");
+document.write("2) " + education[1] + "<br>");
+document.write("3) " + education[2] + "<br>");
+document.write("4) " + education[3] + "<br>");
+document.write("5) " + education[4] + "<br>");
+document.write("6) " + education[5] + "<br>");
+document.write("7) " + education[6] + "<br>");
+document.write("8) " + education[7] + "<br>");
+
+// for (i = 0; i < education.length; i++) {
+
+//     document.write((i+1) + ") " + education[i] + "<br>");
+// }
---
+++ Assignment3_cnc024937/task2.js
@@ -0,0 +1,15 @@
+
+
+
+var students = ["Michael ", "John ", "Tony "];
+
+var score = ["320", "230", "480"];
+
+document.write("Score of " + students[0] + "is " + score[0] +
+". Percentage: " + (score[0] * 100) / 500 + "% <br>");
+
+document.write("Score of " + students[1]+ "is " + score[1] +
+". Percentage: " + (score[1] * 100) / 500 + "% <br>");
+
+document.write("Score of " + students[2]+ "is " + score[2] +
+". Percentage: " + (score[2] * 100) / 500 + "% <br>");
